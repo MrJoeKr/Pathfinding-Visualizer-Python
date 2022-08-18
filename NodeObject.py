569b0b0ed5e9aee89cd35ff1222ca5aa214d4a23
MrJoeKr <kraus.jozef225@gmail.com>
2022-08-18 14:16:38 +0200

    Add draw_display function

--- NodeObject.py
+++ NodeObject.py
@@ -1,7 +1,9 @@
 import pygame
 
 SQUARE_SIZE = 15
+FOREGROUND_PADDING = 1
 WHITE = (255,255,255)
+BLACK = (0,0,0)
 
 Color = tuple[int, int, int]
 
@@ -14,6 +16,10 @@ class Node():
         self.width = SQUARE_SIZE
         self.color = WHITE
 
+        self.draw_position = \
+            (position[0] * self.width, 
+            position[1] * self.width)
+
         # For A* algorithm
         # f = g + h
         self.g = 0
@@ -24,20 +30,32 @@ class Node():
         # check equality with another node
         return self.position == other.position
 
-    def draw_rect(self, display: pygame.display) -> None:
-        x, y = self.position[0], self.position[1]
+    def draw_display(self, display: pygame.display) -> None:
+        x, y = self.draw_position[0], self.draw_position[1]
+
+        # Background square
         pygame.draw.rect(
-            display, self.color, pygame.Rect(x, y, self.width, self.width)) 
+            display, BLACK,
+            pygame.Rect(x, y, self.width, self.width))
+
+        # Foreground square
+        pygame.draw.rect(
+            display, self.color, 
+            pygame.Rect(
+                x + FOREGROUND_PADDING, 
+                y + FOREGROUND_PADDING,
+                self.width - FOREGROUND_PADDING, 
+                self.width - FOREGROUND_PADDING))
 
     def set_color(self, color: Color) -> None:
         self.color = color
 
     # TO BE REMOVED FUNCTIONS
     def get_rect(self) -> pygame.Rect:
-        x = self.position[1] * 18 + 5 
-        y = self.position[0] * 18 + 5
-        rect = pygame.Rect(x ,y, self.width, self.width)
-        return rect
+        # x = self.position[1] * 18 + 5 
+        # y = self.position[0] * 18 + 5
+        x, y = self.draw_position[0], self.draw_position[1]
+        return pygame.Rect(x ,y, self.width, self.width)
 
     def get_pos(self):
         x = self.position[1] * 18 + 5 
