81278b4fb18576a00138bb9831fe8be7c9a93e4f
MrJoeKr <kraus.jozef225@gmail.com>
2022-08-24 21:35:21 +0200

    Add constants files

---
+++ color_constants.py
@@ -0,0 +1,13 @@
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0,242,0)
+RED = (231,0,0)
+LIGHT_GREEN = (126, 186, 0)
+LIGHT_BLUE = (15, 181, 214)
+BLUE = (94, 182, 202)
+LIGHTER_GREEN = (15, 216, 92)
+DARK_BLUE = (0, 0, 111)
+
+# Color type - RGB
+from typing import Tuple
+Color = Tuple[int, int, int]
---
+++ config_constants.py
@@ -0,0 +1,18 @@
+import color_constants
+
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGTH = 650
+
+# Nodes count
+ROWS = 40
+COLS = 53
+
+# Node properties
+NODE_SIZE = 15
+FOREGROUND_PADDING = 1
+WALL_COLOR = color_constants.BLACK
+NODE_COLOR = color_constants.WHITE
+NODE_BORDER_COLOR = color_constants.BLACK
+
+START_POINT_COLOR = color_constants.GREEN
+END_POINT_COLOR = color_constants.RED
