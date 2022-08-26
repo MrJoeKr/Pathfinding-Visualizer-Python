216d882cf607a293108d50bd5b5f5784295244b8
MrJoeKr <kraus.jozef225@gmail.com>
2022-08-26 13:26:40 +0200

    Add new colors

--- color_constants.py
+++ color_constants.py
@@ -1,11 +1,13 @@
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+GRAY = (44, 62, 80)
 GREEN = (132, 209, 66)
 RED = (231,0,0)
 BLUE = (66, 132, 209)
 LIGHT_BLUE = (66, 170, 209)
 LIGHT_GREEN = (66, 209, 104)
 YELLOW = (237, 239, 36)
+GOLDEN = (253, 216, 53)
 
 # Color type - RGB
 from typing import Tuple
